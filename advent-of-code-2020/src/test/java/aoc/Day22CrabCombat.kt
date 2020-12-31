3f7a0c414468203c0246d039d2ae44210f63d965
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2020-12-31 10:53:10 +0100

    Advent of code 2020, days 19-22

---
+++ advent-of-code-2020/src/test/java/aoc/Day22CrabCombat.kt
@@ -0,0 +1,116 @@
+package aoc
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class Day22CrabCombat {
+
+    @Test
+    fun silverTest() {
+        assertThat(playGame(testInput).score()).isEqualTo(306)
+    }
+
+    @Test
+    fun silver() {
+        assertThat(playGame(taskInput).score()).isEqualTo(306)
+    }
+
+    private fun playGame(input: String): List<Int> {
+        val players = input.split("\n\n").map { player ->
+            player.substringAfter(":").lines().mapNotNull { it.toIntOrNull() }
+        }
+
+        return generateSequence(players) { players ->
+            val drawed = players.map { it.first() }.sortedDescending()
+            players.map {
+                it.run {
+                    if (first() == drawed.first()) {
+                        drop(1).plus(drawed)
+                    } else {
+                        drop(1)
+                    }
+                }
+            }
+        }
+            .first { players -> players.any { it.isEmpty() } }
+            .first { it.isNotEmpty() }
+    }
+
+    private fun List<Int>.score(): Int {
+        return reversed().mapIndexed { index, card -> card * (index + 1) }
+            .sum()
+    }
+
+    private val testInput = """
+        Player 1:
+        9
+        2
+        6
+        3
+        1
+
+        Player 2:
+        5
+        8
+        4
+        7
+        10
+    """.trimIndent()
+
+    private val taskInput = """
+        Player 1:
+        3
+        42
+        4
+        25
+        14
+        36
+        32
+        18
+        33
+        10
+        35
+        50
+        16
+        31
+        34
+        46
+        9
+        6
+        41
+        7
+        15
+        45
+        30
+        27
+        49
+
+        Player 2:
+        8
+        11
+        47
+        21
+        17
+        39
+        29
+        43
+        23
+        28
+        13
+        22
+        5
+        20
+        44
+        38
+        26
+        37
+        2
+        24
+        48
+        12
+        19
+        1
+        40
+    """.trimIndent()
+}
+
